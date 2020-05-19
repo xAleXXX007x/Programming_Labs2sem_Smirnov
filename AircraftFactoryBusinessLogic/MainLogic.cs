8bccb2e0b331a563fd72614092c54d785133fa30
AleXXX_007 <AleXXX_007@inbox.ru>
2020-05-19 14:11:04 +0400

    Исправления

--- AircraftFactoryBusinessLogic/MainLogic.cs
+++ AircraftFactoryBusinessLogic/MainLogic.cs
@@ -48,93 +48,7 @@ namespace AircraftFactoryBusinessLogic
                 throw new Exception("Заказ не в статусе \"Принят\"");
             }
 
-            var aircraft = aircraftLogic.GetElement(order.AircraftId);
-            var stocks = stockLogic.GetList();
-
-            foreach (var part in aircraft.AircraftParts)
-            {
-                int count = 0;
-
-                foreach (var stock in stocks)
-                {
-                    foreach (var stockPart in stock.StockParts)
-                    {
-                        if (stockPart.PartId.Equals(part.PartId))
-                        {
-                            count += stockPart.Count;
-                        }
-                    }
-                }
-
-                if (count < part.Count * order.Count)
-                {
-                    throw new Exception("Недостаточно запчастей \"" + part.PartName + "\" для выполнения заказа");
-                } else
-                {
-                    int partsLeft = part.Count * order.Count;
-
-                    foreach (var stock in stocks)
-                    {
-                        List<StockPartViewModel> delete = new List<StockPartViewModel>();
-
-                        foreach (var stockPart in stock.StockParts)
-                        {
-                            if (partsLeft > 0)
-                            {
-                                if (stockPart.PartId.Equals(part.PartId))
-                                {
-                                    if (stockPart.Count <= partsLeft)
-                                    {
-                                        partsLeft -= stockPart.Count;
-
-                                        stockPart.Count = 0;
-
-                                        if (partsLeft == 0)
-                                        {
-                                            continue;
-                                        }
-                                    }
-                                    else
-                                    {
-                                        stockPart.Count -= partsLeft;
-                                        partsLeft = 0;
-
-                                        continue;
-                                    }
-                                }
-                            }
-                        }
-
-                        if (partsLeft == 0)
-                        {
-                            continue;
-                        }
-                    }
-                }
-            }
-
-            foreach (var stock in stocks)
-            {
-                List<StockPartBindingModel> parts = new List<StockPartBindingModel>();
-
-                foreach (var part in stock.StockParts)
-                {
-                    parts.Add(new StockPartBindingModel
-                    {
-                        Id = part.Id,
-                        StockId = part.StockId,
-                        PartId = part.PartId,
-                        Count = part.Count
-                    });
-                }
-
-                stockLogic.UpdElement(new StockBindingModel
-                {
-                    Id = stock.Id,
-                    StockName = stock.StockName,
-                    StockParts = parts
-                });
-            }
+            stockLogic.WithdrawStock(order);
 
             orderLogic.CreateOrUpdate(new OrderBindingModel
             {
@@ -190,39 +104,6 @@ namespace AircraftFactoryBusinessLogic
                 DateCreate = order.DateCreate,
                 DateImplement = order.DateImplement,
                 Status = OrderStatus.Оплачен
-            });
-        }
-
-        public void RefillStock(StockBindingModel model, StockPartBindingModel partModel)
-        {
-            var stock = stockLogic.GetElement(model.Id);
-            if (stock == null)
-            {
-                throw new Exception("Не найден склад");
-            }
-
-            List<StockPartBindingModel> stockParts = model.StockParts;
-            bool found = false;
-            foreach (StockPartBindingModel stockPart in stockParts)
-            {
-                if (stockPart.PartId == partModel.PartId)
-                {
-                    stockPart.Count += partModel.Count;
-                    found = true;
-                    break;
-                }
-            }
-
-            if (!found)
-            {
-                stockParts.Add(partModel);
-            }
-
-            stockLogic.UpdElement(new StockBindingModel
-            {
-                Id = stock.Id,
-                StockName = stock.StockName,
-                StockParts = stockParts
             });
         }
     }
